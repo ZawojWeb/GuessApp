c32c34c2a3a0cf396d66c62a586922b4a8c95197
Zawoj <zawojweb@gmail.com>
2021-07-21 20:48:29 +0200

    Update StartGameScreen.js

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -34,7 +34,18 @@ const styles = StyleSheet.create({
         width: 300,
         maxWidth: '80%',
         alignItems: 'center',
-        
+        shadowColor: 'black',
+        shadowOffset:{
+            width: 0,
+            height:2 ,
+        },
+        shadowRadius:6,
+        shadowOpacity: 0.26,
+        elevation:20,
+        padding:15 ,
+        marginTop: 20,
+        borderRadius: 10,
+        backgroundColor: 'white',
     },
     title:{
         fontSize:20,
