d691d9823eef20f1fdb5cdd731e38657c47e7c11
Zawoj <zawojweb@gmail.com>
2021-07-23 15:17:06 +0200

    Custom Font init

--- App.js
+++ App.js
@@ -5,10 +5,25 @@ import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
 import GameOverScreen from './screens/GameOverScreen';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
+const fetchFonts = () =>{
+  Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-blod': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+}
+
 
 export default function App() {
   const [userNumber, setuserNumber] = useState()
   const [guessRounds, setGuessRounds] = useState(0)
+  const [dataLoading, setDataLoading] = useState(false)
+
+  if(!dataLoading){
+    return <AppLoading startAsync={fetchFonts} onError={(err) => {console.log(err)}} onFinish={() =>{setDataLoading(true)}}/>
+  }
 
   const configreNewGame = () =>{
     setGuessRounds(0);
